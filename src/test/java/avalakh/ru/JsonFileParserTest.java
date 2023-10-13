6fd187b6d00bc43107014d49488617cbf9e83f06
seleverstov <Maksim.Seleverstov@lukoil.com>
2023-10-13 19:08:10 +0300

    homework11 done 2

--- src/test/java/avalakh/ru/JsonFileParserTest.java
+++ src/test/java/avalakh/ru/JsonFileParserTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonFileParserTest {
-    ClassLoader cl = ZipArchiveInspectFilesTest.class.getClassLoader();
     ObjectMapper mapper = new ObjectMapper();
 
     @Test
